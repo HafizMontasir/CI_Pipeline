2f3b52fadf0289189bc3ef2100bd9d6f8956a9fe
Hafiz Montasir <mishanhafiz23@gmail.com>
2024-09-22 09:22:17 +0000

    fix eslint and prettier issues

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -1,38 +1,43 @@
 module.exports = {
-    ignorePatterns: ['build/', 'dist/', 'node_modules/'],
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    plugins: ['react', 'prettier'],
-    settings: {
-      react: {
-        version: 'detect',
+  ignorePatterns: ['build/', 'dist/', 'node_modules/'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', // Add this line to enable eslint-plugin-prettier and eslint-config-prettier
+  ],
+  plugins: ['react', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        jsxSingleQuote: false,
+        printWidth: 120,
+        parser: 'babel-ts',
       },
-    },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
-    },
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-      jest: true,
-    },
-    rules: {
-      'prettier/prettier': [
-        'error',
-        {
-          singleQuote: true,
-          jsxSingleQuote: false,
-          printWidth: 120,
-          parser: 'babel-ts',
-        },
-      ],
-      // Customize your rules here
-      'react/prop-types': process.env.FAIL_LINT ? 2 : 0,
-      'react/jsx-uses-vars': 'warn',
-    },
-  };
-  
+    ],
+    'react/prop-types': process.env.FAIL_LINT ? 2 : 0,
+    'react/jsx-uses-vars': 'warn',
+    // Disable conflicting rules for Prettier
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+  },
+};
